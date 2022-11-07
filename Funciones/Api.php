97c6f584a3c62737d2bb67f11ef408dc3130ecdb
Brian Mora <89980979+BrianOMora@users.noreply.github.com>
2022-11-07 10:17:20 -0600

    Cambio_en_php_pe

--- Funciones/Api.php
+++ Funciones/Api.php
@@ -2,11 +2,10 @@
 //personajes
 $pe = curl_init();
 
-curl_setopt($ch,CURLOPT_URL,'https://rickandmortyapi.com/api/character');
-
+curl_setopt($pe,CURLOPT_URL,'https://rickandmortyapi.com/api/character');
+curl_setopt($pe,CURLOPT_RETURNTRANSFER, true);
+curl_setopt($pe,CURLOPT_HEADER, 0);
+$datos = curl_exec($pe);
 curl_exec($pe);
-
-if(curl_errno($pe)) echo curl_error($pe);
-
 curl_close($pe);
 ?>
